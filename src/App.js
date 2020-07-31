44e309ddd63232ba19b3b8a1773fcb24738d7549
Betahydroksymaslan <wieczork@gmail.com>
2020-07-31 23:33:38 +0200

    Second coommit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          New App, Hello!
+          New App, Hello Version 2!
         </p>
         <a
           className="App-link"
