82d79063c83a18e00e41c61512e474bd19d7821f
Betahydroksymaslan <wieczork@gmail.com>
2020-07-31 23:14:02 +0200

    Updates

---
+++ precache-manifest.63944ab325ce83c749e7842814bdaaba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c636e0e5575cd6242ab0869b9dd41c54",
+    "url": "/test-pages/index.html"
+  },
+  {
+    "revision": "4675f943a8fd0accdbfa",
+    "url": "/test-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fab3c1cf399127029d0f",
+    "url": "/test-pages/static/js/2.6cb6d0fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-pages/static/js/2.6cb6d0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4675f943a8fd0accdbfa",
+    "url": "/test-pages/static/js/main.d3f6afaa.chunk.js"
+  },
+  {
+    "revision": "42e9e2aad3a5c7ef6eca",
+    "url": "/test-pages/static/js/runtime-main.7aceb70d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
